6b983bbfd9276b96b0d7836a99b0c928993d6ea3
Patryk Koreń <koren.patryk@gmail.com>
2022-09-03 17:37:08 +0200

    front

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,49 +1,76 @@
 import React, {useState, useContext} from 'react';
 import { Context } from '../helper/Store';
+
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Button from '@mui/material/Button';
 
 
 function Inputs() {
     const [state, setState] = useContext(Context);  
+
+
     return(
         <>
             <Box
             component="form"
             sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
+                width: '96vw',
+                m: "2vw",
             }}
             noValidate
             autoComplete="off"
             >
-                <TextField id="outlined-basic" label="Address" variant="outlined" />
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                    label="from"
-                    inputFormat="DD/MM/YYYY"
-                    value={state.startDate}
-                    onChange={(newValue) => {
-                        setState(state => ({...state,startDate: newValue }))
-                        console.log("datefrom ", newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                    label="to"
-                    inputFormat="DD/MM/YYYY"
-                    value={state.endDate}
-                    onChange={(newValue) => {
-                        setState(state => ({...state,endDate: newValue }))
-                        console.log("dateTo" , newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>  
+                <Stack direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}>
+                    <TextField id="outlined-basic" 
+                    label="Address" 
+                    variant="filled" 
+                    sx={{ mt:"4vh" }}/>
+                    <Stack
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                    >
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                            label="from"
+                            inputFormat="DD/MM/YYYY"
+                            value={state.startDate}
+                            onChange={(newValue) => {
+                                setState(state => ({...state,startDate: new Date(newValue)}))
+                                console.log("datefrom ", new Date(newValue));
+                            }}
+                            renderInput={(params) => <TextField variant="filled" {...params} />}
+                            />
+                        </LocalizationProvider>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                            label="to"
+                            inputFormat="DD/MM/YYYY"
+                            value={state.endDate}
+                            onChange={(newValue) => {
+                                setState(state => ({...state,endDate:new Date(newValue + 24*3600*1000)}))
+                                console.log("dateTo" , new Date(newValue + 24*3600*1000));
+                            }}
+                            renderInput={(params) => <TextField variant="filled" {...params} />}
+                            />
+                        </LocalizationProvider>
+                    </Stack>
+                    <Button variant="contained" 
+                    onClick={() => {
+                        console.log("clicked");
+                    }}>
+                        Calculate
+                    </Button>
+                </Stack>
             </Box>
         </>
     );
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -11,30 +11,17 @@ import TableRow from '@mui/material/TableRow';
 const columns = [
   { id: 'walletAddress', label: 'Wallet\u00a0Address', minWidth: 170 },
   { id: 'points', label: 'Points', minWidth: 100 },
-  { id: 'hodlDays', label: 'Total Hodls Days', minWidth: 100 },
+  { id: 'tokenVolumeCount', label: 'token Volume Count', minWidth: 100 },
   { id: 'ownedTokens', label: 'Owned NFTs', minWidth: 100 },
 ];
 
-function createData(walletAddress, points, hodlDays, ownedTokens) {
-  return {walletAddress, points, hodlDays, ownedTokens};
+function createData(walletAddress, points, tokenVolumeCount, ownedTokens) {
+  return {walletAddress, points, tokenVolumeCount, ownedTokens};
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData('0x9D53eDc9d94722Bde4a29631B3c2b7F438fd9cE0', 69,  12, 10),
+  createData('0x0987654321', 420, 0, 1),
 ];
 
 export default function Leaderboard() {
@@ -51,7 +38,7 @@ export default function Leaderboard() {
   };
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+    <Paper sx={{ width: "96vw", overflow: 'hidden', m: "2vw"}}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
